4b1c8474a963228a6a76139afed80b33444a896d
robertcollier4 <robertcollier444@gmail.com>
2019-08-15 16:13:11 +0530

    renamed to MarkSearch, updated README

--- MarkSearch.py
+++ MarkSearch.py
@@ -1,21 +1,22 @@
 import sublime, sublime_plugin
 
-#https://github.com/robertcollier4/MarkFromPane
+#https://github.com/robertcollier4/MarkSearch
 #---------------------------------------------------------------
 class MarksearchFromPane(sublime_plugin.WindowCommand):
 
 	def run(self):
-		self.window.show_input_panel("Marksearch: ", "", self.on_done, None, None)
+		self.window.show_input_panel("MarkSearch: ", "", self.on_done, None, None)
 		pass
 	
 	def on_done(self, text):
 		view = self.window.active_view()
-		# ExistingBookmarks = view.get_regions("bookmarks")
-		view.erase_regions("bookmarks")
+		# ExistingBookmarks = view.get_regions("MarkSearch")
+		view.erase_regions("MarkSearch")
 		# self.view.run_command("clear_bookmarks")
 		RegionsFindResults = view.find_all(text, sublime.IGNORECASE | sublime.LITERAL)
-		sublime.status_message("Marksearch has " + str(len(RegionsFindResults)) + " matches for " + text)
-		view.add_regions("bookmarks", RegionsFindResults, "marks", "cross", sublime.DRAW_OUTLINED | sublime.PERSISTENT) # valid gutter icons are dot, circle, bookmark and cross
+		view.set_status("MarkSearch", text + " - " + str(len(RegionsFindResults)) + " matches")
+		#sublime.status_message("MarkSearch has " + str(len(RegionsFindResults)) + " matches for " + text)
+		view.add_regions("MarkSearch", RegionsFindResults, "marks", "cross", sublime.DRAW_OUTLINED | sublime.PERSISTENT) # valid gutter icons are dot, circle, bookmark and cross
 
 #---------------------------------------------------------------
 class MarksearchFromCaret(sublime_plugin.TextCommand):
@@ -23,25 +24,27 @@ class MarksearchFromCaret(sublime_plugin.TextCommand):
 		view = self.view
 		# 32=space 9=tab 10=newline 13=carriagereturn 
 		whiteChars = (chr(32), chr(9), chr(10), chr(13))
-		SearchTerm = self.view.substr(view.sel()[0])
 
-		for ThisRegion in view.sel():
-			ThisRegionBegin = ThisRegion.begin() - 1
-			while((view.substr(ThisRegionBegin) not in whiteChars) and (ThisRegionBegin >= 0)):
-				ThisRegionBegin -= 1
-			ThisRegionBegin += 1
-
-			ThisRegionEnd = ThisRegion.end()
-			while((view.substr(ThisRegionEnd) not in whiteChars) and (ThisRegionEnd < view.size())):
-				ThisRegionEnd += 1
-
-		SearchTerm = view.substr(sublime.Region(ThisRegionBegin, ThisRegionEnd))
-
-		sublime.active_window().active_view().erase_regions("bookmarks")
+		if(view.sel()[0].a == view.sel()[0].b):
+			for ThisRegion in view.sel():
+				ThisRegionBegin = ThisRegion.begin() - 1
+				while((view.substr(ThisRegionBegin) not in whiteChars) and (ThisRegionBegin >= 0)):
+					ThisRegionBegin -= 1
+				ThisRegionBegin += 1
+	
+				ThisRegionEnd = ThisRegion.end()
+				while((view.substr(ThisRegionEnd) not in whiteChars) and (ThisRegionEnd < view.size())):
+					ThisRegionEnd += 1
+			SearchTerm = view.substr(sublime.Region(ThisRegionBegin, ThisRegionEnd))
+		else:
+			SearchTerm = self.view.substr(view.sel()[0])
+			
+		sublime.active_window().active_view().erase_regions("MarkSearch")
 		# self.view.run_command("clear_bookmarks")
 		RegionsFindResults = sublime.active_window().active_view().find_all(SearchTerm, sublime.IGNORECASE | sublime.LITERAL)
-		sublime.status_message("Marksearch has " + str(len(RegionsFindResults)) + " matches for " + SearchTerm)
-		sublime.active_window().active_view().add_regions("bookmarks", RegionsFindResults, "marks", "cross", sublime.DRAW_OUTLINED | sublime.PERSISTENT) # valid gutter icons are dot, circle, bookmark and cross
+		view.set_status("MarkSearch", SearchTerm + " - " + str(len(RegionsFindResults)) + " match")
+		#sublime.status_message("MarkSearch has " + str(len(RegionsFindResults)) + " matches for " + SearchTerm)
+		sublime.active_window().active_view().add_regions("MarkSearch", RegionsFindResults, "marks", "cross", sublime.DRAW_OUTLINED | sublime.PERSISTENT) # valid gutter icons are dot, circle, bookmark and cross
 
 #---------------------------------------------------------------
 class MarkpersonalLine(sublime_plugin.TextCommand):
@@ -51,7 +54,7 @@ class MarkpersonalLine(sublime_plugin.TextCommand):
 		linenumberSelBegin = view.rowcol(view.sel()[0].begin())[0]
 		linenumberSelEnd = view.rowcol(view.sel()[0].end())[0]
 		sublime.status_message("MarkPersonal Added Line # " + str(linenumberSelBegin+1))
-		oldBookmarks = view.get_regions("MarkPersonal")
+		oldBookmarks = view.get_regions("bookmarks")
 		newBookmarks = []
 		bookmarkFoundOnCaretLine = False
 		for thisbookmark in oldBookmarks:
@@ -66,16 +69,21 @@ class MarkpersonalLine(sublime_plugin.TextCommand):
 		else:
 			sublime.status_message("MarkPersonal Line # " + str(linenumberSelBegin+1))
 			newBookmarks.append(view.sel()[0])
-		sublime.active_window().active_view().add_regions("MarkPersonal", newBookmarks, "bookmarks", "circle", sublime.HIDDEN | sublime.PERSISTENT)
+		sublime.active_window().active_view().add_regions("bookmarks", newBookmarks, "bookmarks", "circle", sublime.HIDDEN | sublime.PERSISTENT)
 
 #---------------------------------------------------------------
 class ClearMarkWithStatus(sublime_plugin.TextCommand):
 	def run(self, edit, key="bookmarks"):
-		sublime.status_message("key=" + str(key) + " Marks Cleared")
-		#view = self.window.active_view()
 		view = sublime.active_window().active_view()
+		#view = self.window.active_view()
+
+		sublime.status_message(str(key) + " mark cleared")
 		view.erase_regions(key)
 		# view.run_command('clear_bookmarks')
+
+		if(key == "MarkSearch"):
+			#print("MarkSearch key matched removing set_status")
+			view.set_status("MarkSearch", "")
 
 #---------------------------------------------------------------
 class MarkGotobyindex(sublime_plugin.TextCommand):
@@ -85,7 +93,10 @@ class MarkGotobyindex(sublime_plugin.TextCommand):
 		RegionsMarksForThisKey = view.get_regions(key)
 
 		if(len(RegionsMarksForThisKey) >= index+1):
-			sublime.status_message("Going to Mark key=" + str(key) + " " + str(index+1))
+			if(key == "MarkSearch"):
+				sublime.status_message(str(index+1) + " Match")
+			else:
+				sublime.status_message(str(index+1) + " Match (" + str(key) + ")")
 			ThisRegion = RegionsMarksForThisKey[index]
 			view.sel().clear()
 			view.sel().add(ThisRegion)
@@ -108,23 +119,35 @@ class MarkGotoNext(sublime_plugin.TextCommand):
 			for IndexThisRegion in range(NumRegionsMarksForThisKey):
 				ThisRegion = RegionsMarksForThisKey[IndexThisRegion]
 				PosBeginThisRegion = ThisRegion.begin()
-				if(PosCaretUser <= PosBeginThisRegion):
-					sublime.status_message("key="+ str(key) + " Going to mark " + str(IndexThisRegion+1))
+				if(PosBeginThisRegion > PosCaretUser):
+					if(key == "MarkSearch"):
+						sublime.status_message(str(IndexThisRegion+1) + " Match")
+					else:
+						sublime.status_message(str(IndexThisRegion+1) + " Match (" + str(key) + ")")
 					view.sel().clear()
 					view.sel().add(ThisRegion)
 					view.show(PosBeginThisRegion)
 					return
-			sublime.status_message("key="+ str(key) + " Did not find after mark=" + str(NumRegionsMarksForThisKey))
+			if(key == "MarkSearch"):
+				sublime.status_message("Did not find any more match")
+			else:
+				sublime.status_message("Did not find any more match (" + str(key) + ")")
 		else: #backward
 			PosCaretUser = view.sel()[0].begin()
 			for IndexThisRegion in reversed(range(NumRegionsMarksForThisKey)):
 				if(RegionsMarksForThisKey[IndexThisRegion].begin() < PosCaretUser):
-					sublime.status_message("key="+ str(key) + " Going to mark " + str(IndexThisRegion+1))
+					if(key == "MarkSearch"):
+						sublime.status_message(str(IndexThisRegion+1) + " Match")
+					else:
+						sublime.status_message(str(IndexThisRegion+1) + " Match (" + str(key) + ")")
 					view.sel().clear()
 					view.sel().add(RegionsMarksForThisKey[IndexThisRegion])
 					view.show(RegionsMarksForThisKey[IndexThisRegion].begin())
 					return
-			sublime.status_message("key="+ str(key) + " Did not find before mark 1")
+			if(key == "MarkSearch"):
+				sublime.status_message("Did not find any match before")
+			else:
+				sublime.status_message("Did not find any match before (" + str(key) + ")")
 
 #---------------------------------------------------------------
 # JUNKYARD
--- README.md
+++ README.md
@@ -1,18 +1,33 @@
-# MarkFromPane
-Bookmark words and lines with different classes from a panel and within editor. SublimeText ST3.
+# MarkSearch
 
-## 2 Classes of Marks - Marksearches and Markpersonal
-This allows you to set 2 classes of marks - Marksearch and Markpersonal.  
-Marksearch are outlined around words and shown as outlines around words and in gutter  
-Markpersonal are line based and shown in gutter only  
+For SublimeText ST3.
+![Screenshot MarkSearch ST3](screenshot-marksearch.gif)
 
-Functions Provided:  
-**MarksearchFromPane** - Clears the old Marksearch, and sets a Marksearch from a pane like a find pane. This is marked by a outline around the word searched for. And a arrow for the line on the gutter.  
-**MarksearchFromCaret** - Clears the old Marksearch, and sets a Marksearch from the word your caret is on. This is marked by a outline around the word searched for. And a arrow for the line on the gutter.  
-**MarkpersonalLine** - Sets a Markpersonal for the current line you are on. This is marked by a circle for the line on the gutter.  
-**ClearMarksWithStatus** - Clears the old mark of key  
-**MarkGotobyindex** - Go to mark of key of specified index  
-**MarkGotoNext** - Go to previous or next mark of key  
+## MarkSearch
+
+Outlines all search results and then you can iteratively go through them like Prev Result and Next Result while keeping them outlined.
+
+Each search result is kept outlined so you can see them and keep seeing them visually. The SublimeText default interface does not continuously show you all search results highlighted when you are using Prev Result and Next Result and loses the highlight. The visual indication is kept via setting a marker for each search result with the outline mode on. You can then go up and down through each search result with "Ctrl+[" and "Ctrl+]".
+
+* <kbd>ctrl+f</kbd> Do a MarkSearch by typing a search term in the pane (all search results will be outlined)
+* <kbd>ctrl+h</kbd> Do a MarkSearch on highlighted cursor text (all search results will be outlined)
+* <kbd>ctrl+\\</kbd> Go to the first MarkSearch result
+* <kbd>ctrl+]</kbd> Go to the next MarkSearch result (down)
+* <kbd>ctrl+[</kbd> Go to the previous MarkSearch result (up)
+* <kbd>ctrl+u</kbd> Clear MarkSearch results (and outlines)
+
+The way to use this quickly with a keyboard intuitive workflow is: <kbd>Ctrl+F</kbd> --> <kbd>type in Search Term</kbd> --> <kbd>Ctrl+|</kbd> --> make changes --> <kbd>Ctrl+]</kbd> --> Repeat <kbd>Ctrl+]</kbd>
+
+## MarkPersonal
+
+MarkPersonal Lines are line based marks. You can then jump back to them via Ctrl+number.
+
+This is useful to go back to lines you were previously working on after doing some search based from replacements.
+
+* <kbd>ctrl+shift+1</kbd> Sets a Markpersonal for the current line you are on. This is marked by a circle for the line on the gutter.  
+* <kbd>ctrl+1</kbd> Go to Line MarkPersonal 1
+* <kbd>ctrl+2</kbd> Go to Line MarkPersonal 2
+* <kbd>ctrl+3-9</kbd> Go to Line MarkPersonal 3-9 corresponding to the number key
 
 ## Key Bindings Configuration
 Since these are replacing your basic find keys this package does not automatically overwrite your existing key bindings. You must choose to add the keybindings yourself for your specific OS.
